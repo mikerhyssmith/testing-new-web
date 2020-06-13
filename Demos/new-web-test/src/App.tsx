b0fd765f70cc56e60f703ec18c205f88aab67255
mikerhyssmith <mikerhyssmith@googlemail.com>
2020-06-13 10:49:54 +0100

    add routing

--- Demos/new-web-test/src/App.tsx
+++ Demos/new-web-test/src/App.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import './App.css';
 import {Grommet} from 'grommet';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import {theme} from './theme';
 import {NetworkTester} from './components/NetworkTester/NetworkTester';
-import { VRWorld } from "./components/VRWorld/VRWorld";
+import {VRWorld} from './components/VRWorld/VRWorld';
 
 import './App.css';
 
 function App() {
   return (
-    <Grommet full theme={theme as any} themeMode="dark">
-      <div className="App">
-        <VRWorld />
-        <NetworkTester />
-      </div>
-    </Grommet>
+    <Router>
+      <Grommet full theme={theme as any} themeMode="dark">
+        <div className="App">
+          <Switch>
+            <Route path="/VR">
+              <VRWorld />
+            </Route>
+            <Route path="/network">
+              <NetworkTester />
+            </Route>
+            <Route path="/">
+              <Link to="/VR">VR Demo</Link>
+              <Link to="/network">Network Tester</Link>
+            </Route>
+          </Switch>
+        </div>
+      </Grommet>
+    </Router>
   );
 }
 
