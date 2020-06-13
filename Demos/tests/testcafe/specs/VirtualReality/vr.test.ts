c06f63d33791e467e38cf5e5daf15a0b72fa28d8
mikerhyssmith <mikerhyssmith@googlemail.com>
2020-06-13 16:02:54 +0100

    work on testing VR

--- Demos/tests/testcafe/specs/VirtualReality/vr.test.ts
+++ Demos/tests/testcafe/specs/VirtualReality/vr.test.ts
@@ -1,8 +1,14 @@
 import {Selector} from 'testcafe';
 
-fixture`VR Scene`.page`http://192.168.1.101:3000/VR`;
+fixture`VR Scene`.page`http://192.168.1.101:3000/VR`.clientScripts({path: './XRPolyfill.js'});
 
-test('Check initial view (with Percy)', async t => {
+//
+test.only('VR Operations', async t => {
   // Need to try this with ngrok
   await t.click('.a-enter-vr-button');
+  await t.eval(() =>
+    window.dispatchEvent(
+      new CustomEvent('webxr-pose', {detail: {position: [0.0], quaternion: [0, 20]}})
+    )
+  );
 });
